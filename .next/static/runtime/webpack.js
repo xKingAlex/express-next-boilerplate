3f5b7a37ce5c314b119ec62c7d101460248f43bc
Alex Bailey <alexwbailey7@gmail.com>
2020-02-07 15:35:47 -0600

    adding config files

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6e776d07f8061f5d39a1";
+/******/ 	var hotCurrentHash = "3b19b60ef53bae37f5ca";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
